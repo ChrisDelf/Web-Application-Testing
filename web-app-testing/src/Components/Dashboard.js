be3970fc2895a94be47015be7d09e63152c8cd24
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-08-08 16:05:15 -0400

    strikes and balls are working

--- web-app-testing/src/Components/Dashboard.js
+++ web-app-testing/src/Components/Dashboard.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 
-const Dashboard = props => {
+const Dashboard = ({striker, ball}) => {
+
   return (
     <Card>
-    <Button>Strike</Button>
-    <Button>Ball</Button>
+    <Button onClick ={striker}>Strike</Button>
+    <Button onClick ={ball}>Ball</Button>
     <Button>Foul</Button>
     <Button>Hit</Button>
     </Card>
--- web-app-testing/src/Components/Display.js
+++ web-app-testing/src/Components/Display.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '@material-ui/core/Card';
 import Paper from '@material-ui/core/Paper';
 import Dashboard from './Dashboard';
 
 const Display = props => {
-  const []
+  const [strike, setStrike] = useState(0);
+  const [ball, setBall] = useState(0);
+  const [foul, setFoul] = useState(0);
+  const [hit, setHit] = useState(0);
+
+  const handleStriker = () => {
+    if (strike >= 3) {
+      setStrike(0);
+      setBall(0)
+    } else {
+      setStrike(strike + 1);
+    }
+  };
+
+  const handleBall = () => {
+    if (ball >= 4) {
+      setBall(0)
+      setStrike(0);
+    } else {
+      setBall(ball + 1);
+    }
+  };
+
   return (
     <Paper>
-      <Card>Strikes</Card>
-      <Card>ball</Card>
+      <Card>Strikes {strike}</Card>
+      <Card>ball {ball}</Card>
       <Card>foul</Card>
-    <Card>hit</Card>
-    <Dashboard/>
+      <Card>hit</Card>
+      <Dashboard key={strike.id} striker={handleStriker} ball={handleBall} />
     </Paper>
-
   );
 };
 
